d607df4c48603ae3ed6b19d936d445d8dc381e8b
Giovanna Cabral <127431201+eugiovannacabral@users.noreply.github.com>
2025-10-29 22:37:05 -0300

    Implement Pokemon classes with attack methods

---
+++ Relatório 10/Exercício 1.js
@@ -0,0 +1,72 @@
+class Pokemon {
+    #vida = 0;
+
+    constructor(nome, tipo, vidaInicial) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.#vida = vidaInicial;
+    }
+
+    
+    getVida() {
+        return this.#vida;
+    }
+
+    receberDano(dano) {
+      const vidaAntes = this.#vida;     
+      this.#vida -= dano;               
+      
+      if (this.#vida < 0) 
+      this.#vida = 0;
+      const vidaPerdida = vidaAntes - this.#vida;  
+      
+    console.log(`${this.nome} perdeu ${vidaPerdida} de vida!`);
+}
+
+  atacar(alvo) {
+    console.log(`${this.nome} atacou ${alvo.nome}`);
+    alvo.receberDano(2); //dano fixo de menos 2 pontos no adversario
+  }
+
+}
+class PokemonFogo extends Pokemon { //HERANÇA DA CLASSE MAE
+    
+    constructor(nome, vidaInicial, bonusAtaque) {
+    super(nome, "Fogo", vidaInicial);
+    this.bonusAtaque = bonusAtaque;
+  }
+
+  atacar(alvo) {
+    const dano = 5 + this.bonusAtaque; //é o dano fixo desse pokemon mais o bonus de dano
+    
+    console.log(`${this.nome} lançou fogo em ${alvo.nome}!`);
+    alvo.receberDano(dano);
+  }
+}
+
+class PokemonAgua extends Pokemon {
+    #curaBase; 
+
+  constructor(nome, vidaInicial, curaBase) {
+    
+    super(nome, "Água", vidaInicial);
+    this.#curaBase = curaBase;
+  }
+
+  atacar(alvo) {
+    const dano = 15;
+    
+    console.log(`${this.nome} deixou ${alvo.nome} com osmose de tanta água!`);
+    alvo.receberDano(dano);
+}
+
+}
+
+const pk1 = new PokemonFogo("Foguinho", 40, 10);
+const pk2 = new PokemonAgua("Aquagirl", 78, 3);
+
+pk1.atacar(pk2);
+pk2.atacar(pk1);
+
+console.log(`\n ${pk1.nome} possui ${pk1.getVida()} de vida`);
+console.log(`${pk2.nome} possui ${pk2.getVida()} de vida`);
